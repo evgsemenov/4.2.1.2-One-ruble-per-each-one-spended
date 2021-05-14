c50cd0eacd9282d5e1551c19e1092b0435b1160a
Evgenii Semenov <eo.semenoff@gmail.com>
2021-05-14 14:45:08 +0300

    Readme changed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int balance = 35;
+        int refill = 1001;
+        int bonusCoefficient = 100;
+        // bonusCoefficient - сколько рублей нужно потратить, чтобы получить 1 бонусный рубль
+        int bonus;
+        if (refill > 1000) {
+            bonus = (int) (refill / bonusCoefficient);
+        } else {
+            bonus = 0;
+        }
+        int totalBalance = balance + refill + bonus;
+        System.out.println("Итоговый баланс: " + totalBalance + " рублей");
+        System.out.println("Бонус составил: " + bonus + " рублей");
+
+
+    }
 }
